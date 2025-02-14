aef0f42869fe349b910f2e1f5d1da0533be7a1d6
Hiennguyen195 <nguyenhien19052000@gmail.com>
2025-02-14 13:23:02 +0700

    JavaTraining

--- src/main/java/com/example/javatrainingbai1/Cart.java
+++ src/main/java/com/example/javatrainingbai1/Cart.java
@@ -2,37 +2,29 @@ package com.example.javatrainingbai1;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Scanner;
 
 public class Cart {
     private int product_quantity;
     private List<Product> cart_products = new ArrayList<>();
 
+    // Constructor:
     public Cart() {
     }
-
-    public Cart(List<Product> cart_products) {
+    public Cart(int product_quantity, List<Product> cart_products) {
+        this.product_quantity = product_quantity;
         this.cart_products = cart_products;
     }
-
+    // Getter and Setter:
     public int getProduct_quantity() {
         return product_quantity;
     }
-
     public void setProduct_quantity(int product_quantity) {
         this.product_quantity = product_quantity;
     }
-
     public List<Product> getCart_products() {
         return cart_products;
     }
-
     public void setCart_products(List<Product> cart_products) {
-        this.cart_products = cart_products;
-    }
-
-    public Cart(int product_quantity, List<Product> cart_products) {
-        this.product_quantity = product_quantity;
         this.cart_products = cart_products;
     }
 }
